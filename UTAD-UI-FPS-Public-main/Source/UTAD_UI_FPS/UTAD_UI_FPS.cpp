272f5c7076c8a2a558f2b881ba815fe3a9390b1e
Ounsesa <carlos.rocal01@gmail.com>
2024-02-17 12:46:10 +0100

    Initn't

--- UTAD-UI-FPS-Public-main/Source/UTAD_UI_FPS/UTAD_UI_FPS.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UTAD_UI_FPS.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UTAD_UI_FPS, "UTAD_UI_FPS" );
- 
